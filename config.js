ec462edd16ff566016ba05664eef48d69d3eae76
snooopyc <snooopyc@gmail.com>
2019-04-13 18:46:54 -0700

    Update config.js

--- config.js
+++ config.js
@@ -6,12 +6,6 @@ module.exports = {
   showHeaderImage: true,
   showShareButtons: false,
   postsPerPage: 5, // Required
-  social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
-  },
+
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
 };
